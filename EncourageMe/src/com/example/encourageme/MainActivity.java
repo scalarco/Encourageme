ca10f2cb43d21e6f4634bda2aec6fe99d7f09305
wesworth <sean.calarco@gmail.com>
2013-01-17 21:34:55 -0500

    Added welcomepopup

--- EncourageMe/src/com/example/encourageme/MainActivity.java
+++ EncourageMe/src/com/example/encourageme/MainActivity.java
@@ -1,11 +1,48 @@
 package com.example.encourageme;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	SharedPreferences mPrefs;
+	final String welcomeScreenShownPref = "welcomeScreenShown";
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+
+	    mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+	    // second argument is the default to use if the preference can't be found
+	    Boolean welcomeScreenShown = mPrefs.getBoolean(welcomeScreenShownPref, false);
+
+	    if (!welcomeScreenShown) {
+	        // here you can launch another activity if you like
+	        // the code below will display a popup
+
+	        String whatsNewTitle = getResources().getString(R.string.whatsNewTitle);
+	        String whatsNewText = getResources().getString(R.string.whatsNewText);
+	        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle(whatsNewTitle).setMessage(whatsNewText).setPositiveButton(
+	                R.string.ok, new DialogInterface.OnClickListener() {
+	                    public void onClick(DialogInterface dialog, int which) {
+	                        dialog.dismiss();
+	                    }
+	                }).show();
+	        SharedPreferences.Editor editor = mPrefs.edit();
+	        editor.putBoolean(welcomeScreenShownPref, true);
+	        editor.commit(); // Very important to save the preference
+	    }
+
+	}
+	/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +55,5 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+    */
 }
